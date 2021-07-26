18492de4ba287b00979694f9876eba8c6a346620
mmelgar0468 <mmelgar@arquiea.com>
2021-07-26 17:26:20 +0200

    commit

--- midi_creator.py
+++ midi_creator.py
@@ -12,33 +12,43 @@ def write_wave(midi_file, filename: str = "out.midi"):
         mf.writeFile(midi_file)
 
 
-# create your MIDI object
-mf = MIDIFile(1)  # only 1 track
-track = 0  # the only track
+if __name__ == '__main__':
+    # create your MIDI object
+    track = 0  # Track numbers are zero-origined
+    channel = 0  # MIDI channel number
+    pitch = 60  # MIDI note number
+    time = 0  # In beats
+    duration = 1  # In beats
+    volume = 100  # 0-127, 127 being full volume
 
-time = 0  # start at the beginning
-mf.addTrackName(track, time, "Sample Track")
-mf.addTempo(track, time, 120)
+    mf = MIDIFile(1)  # only 1 track
+    track = 0  # the only track
 
-# add some notes
-channel = 0
-volume = 100
+    time = 0  # start at the beginning
+    mf.addTrackName(track, time, "Sample Track")
+    mf.addTempo(track, time, 120)
+    program = 86
+    mf.addProgramChange(track, channel, time, program)
 
-pitch = 60  # C4 (middle C)
-time = 0  # start on beat 0
-duration = 1  # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
+    # add some notes
+    channel = 0
+    volume = 100
 
-pitch = 64  # E4
-time = 2  # start on beat 2
-duration = 1  # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
+    pitch = 60  # C4 (middle C)
+    time = 0  # start on beat 0
+    duration = 1  # 1 beat long
+    mf.addNote(track, channel, pitch, time, duration, volume)
 
-pitch = 67  # G4
-time = 4  # start on beat 4
-duration = 1  # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
+    pitch = 64  # E4
+    time = 2  # start on beat 2
+    duration = 1  # 1 beat long
+    mf.addNote(track, channel, pitch, time, duration, volume)
 
-# write it to disk
-with open("output.mid", 'wb') as outf:
-    mf.writeFile(outf)
+    pitch = 67  # G4
+    time = 4  # start on beat 4
+    duration = 1  # 1 beat long
+    mf.addNote(track, channel, pitch, time, duration, volume)
+
+    # write it to disk
+    with open("output.mid", 'wb') as outf:
+        mf.writeFile(outf)
