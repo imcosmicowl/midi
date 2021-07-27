a9452dc997387dfe0cf7b1ff8bb07617718a5db0
mmelgar0468 <mmelgar@arquiea.com>
2021-07-27 15:57:30 +0200

    commit

--- midi_creator.py
+++ midi_creator.py
@@ -2,20 +2,26 @@ from midiutil.MidiFile import MIDIFile
 from midi2audio import FluidSynth
 
 
-def write_midi_file(midi_file, filename: str = "out.midi"):
+def write_midi_file(midi_object, filename: str = "out.midi"):
     """
     writes midi into a file
-    :param midi_file:
+    :param midi_object:
     :param filename:
     :return:
     """
-    with open(filename, "wb") as midi_file:
-        mf.writeFile(midi_file)
+    with open(filename, "wb") as outf:
+        midi_object.writeFile(outf)
 
 
 def midi2wave(midifile, outputfile='output.wav'):
     fs = FluidSynth()
     fs.midi_to_audio(midifile, outputfile)
+
+
+def pleasant_loop(midi_object, loop_duration, length):
+
+    for step in range(loop_duration):
+        midi_object.addNote(track, channel, 108, step, length, volume)
 
 
 if __name__ == '__main__':
@@ -28,6 +34,14 @@ if __name__ == '__main__':
     mf = MIDIFile(1)  # only 1 track
     mf.addTrackName(track, time, "Sample Track")
     mf.addTempo(track, time, 120)
+    # 89 muy mystic
+    # 90 = q 89 xro un poco reber y expanded
+    # 93 chachistic but short
+    # 94 cool but short
+    # 95 most mystic & relaxing
+    # 73 piccolo
+    # 52 mystic but in mid range freq
+
     program = 95 # instrument
     mf.addProgramChange(track, channel, time, program)
 
@@ -36,16 +50,15 @@ if __name__ == '__main__':
 
     time = 0  # start on beat 0
     duration = 16  # In beats
-    mf.addNote(track, channel, 97, time, duration, volume)
-    mf.addNote(time, channel, 12, time, duration, volume)
+    mf.addNote(track, channel, 108, time, duration, volume)
+    mf.addNote(time, channel, 98, time, duration, volume)
 
     time = 16
     duration = 16
-    mf.addNote(track, channel, 104, time, duration, volume)
-    mf.addNote(track, channel, 20, time, duration, volume)
+    mf.addNote(track, channel,115, time, duration, volume)
+    mf.addNote(track, channel, 105, time, duration, volume)
 
     # write it to disk
-    with open("output.mid", 'wb') as outf:
-        mf.writeFile(outf)
+    write_midi_file(mf)
 
-    midi2wave("output.mid")
+    midi2wave("out.midi")
